8e3b93ca8315f579ae149d4baa0a24f8a1809d9e
Imran Saee <imran.saee@gmail.com>
2020-06-06 17:09:59 +0500

    pushed

---
+++ Class Practice/Class10 11052020/C10_app.js
@@ -0,0 +1,17 @@
+// var a = prompt("Enter you age", "Your age is:");
+// var b = a + 10;
+// alert(b)
+
+var age = +prompt("enter you age")
+if (age < 18) 
+{
+    alert("You are under age")
+}
+elseif(age > 18)
+{
+    alert("You are not underage")
+}
+else
+{
+    alert("You are good to go")
+}
